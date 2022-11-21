32aae53da08c5b5cf3e434028aa02ac6c77ce366
Dan Koskiranta <G00397054@atu.ie>
2022-11-21 09:52:19 +0000

    Added new code

--- src/main/java/ie/atu/week8/CreateFile.java
+++ src/main/java/ie/atu/week8/CreateFile.java
@@ -1,4 +1,24 @@
 package ie.atu.week8;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class CreateFile {
+    public static void main(String[] args) {
+        {
+            File myFile = new File ("MyFile.txt");
+            System.out.println("My file is located at: " + myFile.getAbsolutePath());
+            try
+            {
+                FileWriter fw = new FileWriter("MyFile.txt");
+                fw.write("New Data");
+                fw.close();
+            }
+            catch(IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
 }
