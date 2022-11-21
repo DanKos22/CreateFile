84bb59669963ccb030f63e93ee683d24bb185098
Dan Koskiranta <G00397054@atu.ie>
2022-11-21 11:02:51 +0000

    Read the data from the file

--- src/main/java/ie/atu/week8/CreateFile.java
+++ src/main/java/ie/atu/week8/CreateFile.java
@@ -1,4 +1,32 @@
 package ie.atu.week8;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class CreateFile {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please enter the filename: ");
+        String filename = keyboard.nextLine();
+
+        // Open the file
+        try(Scanner inputFile = new Scanner(new File(filename)))
+        {
+            // Read until the end of the file
+            while(inputFile.hasNext())
+            {
+                // Reading a line from the file
+                String str = inputFile.nextLine();
+                System.out.println(str);
+            }
+            // Close the file when done
+            inputFile.close();
+        }
+        catch(FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
